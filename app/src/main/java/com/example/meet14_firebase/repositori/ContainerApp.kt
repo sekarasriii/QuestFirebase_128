2ae4a4e85bcc4bf4bc068ba904a196a9e90afd4b
sekarasriii <sekareeesh@gmail.com>
2026-01-02 14:22:24 +0700

    tambah defaultcontainerapp dengan inisialisasi lazy repository

--- app/src/main/java/com/example/meet14_firebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/meet14_firebase/repositori/ContainerApp.kt
@@ -7,3 +7,8 @@ interface ContainerApp{
     val repositorySiswa: RepositorySiswa
 }
 
+class DefaultContainerApp: ContainerApp{
+    override val repositorySiswa: RepositorySiswa by lazy{
+        FirebaseRepositorySiswa()
+    }
+}
