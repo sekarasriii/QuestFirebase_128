fcc8c38959b48f004d8256e16cb56f2018871b82
sekarasriii <sekareeesh@gmail.com>
2026-01-02 15:09:27 +0700

    menambahkan destinasi detail dengan argumen id siswa

---
+++ app/src/main/java/com/example/meet14_firebase/view/route/DestinasiDetail.kt
@@ -0,0 +1,11 @@
+package com.example.meet14_firebase.view.route
+
+import com.example.meet14_firebase.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
