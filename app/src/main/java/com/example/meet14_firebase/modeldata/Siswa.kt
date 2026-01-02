9dc39355af81eacb6cd7271ec592f13463d58581
sekarasriii <sekareeesh@gmail.com>
2026-01-02 13:57:02 +0700

    menambahkan definisi struktur model siswa dan detailsiswa

---
+++ app/src/main/java/com/example/meet14_firebase/modeldata/Siswa.kt
@@ -0,0 +1,15 @@
+package com.example.meet14_firebase.modeldata
+
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+data class DetailSiswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
