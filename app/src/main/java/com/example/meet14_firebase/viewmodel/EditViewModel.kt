9aef8fa63c8e46c81d2be17c89c6a2fe56602cb2
sekarasriii <sekareeesh@gmail.com>
2026-01-08 18:52:55 +0700

    Menambahkan logic update state form dan validasi input sebelum data disimpan

--- app/src/main/java/com/example/meet14_firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/meet14_firebase/viewmodel/EditViewModel.kt
@@ -29,4 +29,15 @@ RepositorySiswa): ViewModel() {
                 .toUiStateSiswa(true)
         }
     }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 }
