db82ebb2267cab857c825c67db8c80aecad9241f
sekarasriii <sekareeesh@gmail.com>
2026-01-08 17:49:42 +0700

    Menambahkan sealed interface StatusUIDetail untuk mengatur state UI Detail Siswa (Loading, Success, Error).

--- app/src/main/java/com/example/meet14_firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/meet14_firebase/viewmodel/DetailViewModel.kt
@@ -1,1 +1,21 @@
+@file:OptIn(InternalSerializationApi::class)
+
 package com.example.meet14_firebase.viewmodel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import  kotlinx.serialization.InternalSerializationApi
+import com.example.meet14_firebase.modeldata.Siswa
+import com.example.meet14_firebase.repositori.RepositorySiswa
+import com.example.meet14_firebase.view.route.DestinasiDetail
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+import androidx.compose.runtime.setValue
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
