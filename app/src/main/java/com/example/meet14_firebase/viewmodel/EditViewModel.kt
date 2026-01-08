617df4e60a4efdb65c0e4c08035277102f0d33ba
sekarasriii <sekareeesh@gmail.com>
2026-01-08 18:47:19 +0700

    Mengambil data siswa dari repository lalu mengonversinya ke UIStateSiswa saat ViewModel dibuat.

--- app/src/main/java/com/example/meet14_firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/meet14_firebase/viewmodel/EditViewModel.kt
@@ -22,4 +22,11 @@ RepositorySiswa): ViewModel() {
     private val idSiswa: Long =
         savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
             ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
 }
