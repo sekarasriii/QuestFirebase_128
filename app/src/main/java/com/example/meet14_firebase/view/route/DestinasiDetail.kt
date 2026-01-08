9efd9e08dba8871d051225a3665a5ba2b9ba2dc7
sekarasriii <sekareeesh@gmail.com>
2026-01-08 19:39:13 +0700

    Memperbaharui code destinasi detail pada variabel itemIdArg

--- app/src/main/java/com/example/meet14_firebase/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/meet14_firebase/view/route/DestinasiDetail.kt
@@ -5,6 +5,6 @@ import com.example.meet14_firebase.R
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
-    const val siswaIdArg = "siswaId"
-    val routeWithArgs = "$route/{$siswaIdArg}"
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
