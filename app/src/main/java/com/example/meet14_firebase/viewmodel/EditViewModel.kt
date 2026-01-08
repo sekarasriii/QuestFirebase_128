547667626237cc97345398f6700f4c1bbedb3659
sekarasriii <sekareeesh@gmail.com>
2026-01-08 18:44:43 +0700

    Membuat EditViewModel dan menyiapkan state awal uiStateSiswa untuk kebutuhan form edit siswa.

--- app/src/main/java/com/example/meet14_firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/meet14_firebase/viewmodel/EditViewModel.kt
@@ -1,1 +1,21 @@
 package com.example.meet14_firebase.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.meet14_firebase.modeldata.UIStateSiswa
+import com.example.meet14_firebase.repositori.RepositorySiswa
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import com.example.meet14_firebase.view.route.DestinasiDetail
+import  androidx.lifecycle.viewModelScope
+import com.example.meet14_firebase.modeldata.toDataSiswa
+import kotlinx.coroutines.launch
+import com.example.meet14_firebase.modeldata.DetailSiswa
+import androidx.compose.runtime.setValue
+import com.example.meet14_firebase.modeldata.toUiStateSiswa
+
+class EditViewModel (savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+}
